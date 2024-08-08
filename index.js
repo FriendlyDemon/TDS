8102a05648757d44b27d689b238ff9684515e0df
Dev Jeff Alves <lucas-bbica@educar.rs.gov.br>
2024-08-08 16:40:20 -0300

    y

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
 // senac.blackboard
 
 // *V*>*>*A*VVV<*
+
+/*
+grep -i pikachu ./pokemon.txt
+grep -i -B2 pikachu ./pokemon.txt
+grep -i -A2 pikachu ./pokemon.txt
+head -n152 ./pokemon.txt
+tail -n100 ./pokemon.txt
+*/
+
+// git clone [url]
+// git commit -m ""
