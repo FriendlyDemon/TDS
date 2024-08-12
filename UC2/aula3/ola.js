7166c6aca3b1287d7d9f169a0f8afa14bcbecbec
senacrs <henriquemichel@live.com>
2024-08-12 16:24:03 -0300

    y

--- UC2/aula3/ola.js
+++ UC2/aula3/ola.js
@@ -1,2 +1,6 @@
+/*
 const nome = "Fulana", idade=21;
 console.log('Olá','Meu nome é :',nome,'e eu tenho',idade,'anos')
+*/
+let nome = 'Henrique',sobrenome = 'Michel', idade = 30, estudante = true,souEstudante=null;if (estudante){souEstudante ='sou'}else{souEstudante = 'não sou'};
+console.log(`Meu nome é ${nome} ${sobrenome}, eu tenho ${idade} anos e eu ${souEstudante} estudante`)
