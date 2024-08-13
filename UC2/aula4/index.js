61cb06879c3db1a77bcfedddfe9dd6e771a24b04
senacrs <henriquemichel@live.com>
2024-08-13 16:56:33 -0300

    y

--- UC2/aula4/index.js
+++ UC2/aula4/index.js
@@ -1,6 +1,6 @@
-rl=require('readline-sync')
+const rl=require('readline-sync');
 function temp(C){
-    F=(C*9.5)+(32)
+    F=((C*9.5)+(32))
     console.log(F)
 }
-temp(Number().rl.question('Insira o numero em Celsius: '))
+temp(Number(rl.question('Insira o numero em Celsius: ')))
