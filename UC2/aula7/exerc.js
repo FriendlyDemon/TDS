365ef0ee87d8880cb80f0e869f196fe6b7d308d3
senacrs <henriquemichel@live.com>
2024-08-20 16:13:08 -0300

    y

--- UC2/aula7/exerc.js
+++ UC2/aula7/exerc.js
@@ -1,5 +1,12 @@
-nome
-idade
-midade=idade>=18
-futuro=idade+26
-console.log('nome é',nome,'idade é',idade)
+const bool1 = true, bool2 = false, bool3 = !bool2;
+
+let resultado = bool1 && bool2;
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3
+console.log("b. ", resultado)
+
+resultado = !resultado && (bool1 || bool2)
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)
