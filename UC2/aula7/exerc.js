d4774a7779981165580c3d6d6c59df97af202b76
senacrs <henriquemichel@live.com>
2024-08-20 15:53:20 -0300

    y

---
+++ UC2/aula7/exerc.js
@@ -0,0 +1,5 @@
+nome
+idade
+midade=idade>=18
+futuro=idade+26
+console.log('nome é',nome,'idade é',idade)
--- UC2/aula7/index.js
+++ UC2/aula7/index.js
@@ -1,5 +1,21 @@
-v1=true
-v2=true
+a1=true
+a2=true
 
-r1=v1&&v2
-console.log(r1)
+r=a1&&a2
+console.log(r)
+
+a=true
+b=false
+c=true
+
+r1=a&&b
+r2=b&&c
+r3=a&&c
+r4=a&&b&&c
+console.log(`${r1},${r2},${r3},${r4}`)
+
+r5=a||b
+r6=b||c
+r7=a||c
+r8=a||b||c
+console.log(`${r5},${r6},${r7},${r8}`)
