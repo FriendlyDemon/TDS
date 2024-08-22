05d9da3940598cb2590a78cc3dbd9fd63d76fba9
senacrs <henriquemichel@live.com>
2024-08-22 17:24:07 -0300

    y

---
+++ UC2/aula8/exerc.js
@@ -0,0 +1,5 @@
+a=[1,2,3,4,5,6]
+console.log(a.length)
+a.push(7)
+a.splice(3,2)
+console.log(a.length)
--- UC2/aula8/index.js
+++ UC2/aula8/index.js
@@ -12,7 +12,32 @@ const frutas = ['abacate', 'banana', 'tomate', 'milho', 'uva', 'morango'],
 fruta=frutas[4]
 console.log(fruta)
 */
-/* array exerc
-cachorros=['bulldog','pitbull','pug','golden retriever','chow-chow']
+/* array exerc1
+const cachorros=['bulldog','pitbull','pug','golden retriever','chow-chow']
 console.log(cachorros[require('readline-sync').keyInSelect(cachorros,'Escolha uma raca de cachorro')])
+*/
+/* array exerc2
+const pokemons=['bulbasaur','ivysaur','venusaur','charmander','charmeleon','charizard','squirtle','wartortle','blastoise']
+console.log(pokemons[0])
+*/
+/* array push()
+const numeros=[1,2,3]
+console.log(numeros)
+numeros.push(4)
+console.log(numeros)
+numeros.push(5,6,7,8)
+console.log(numeros)
+*/
+/* array pop()
+const peixes=['palhaço','mandarin','esturjão']
+console.log(peixes)
+peixes.pop()
+console.log(peixes)
+*/
+/* array splice()
+letras=['a','b','c','d','e','f','g','h']
+letras.splice(2,1)
+console.log(letras)
+letras.splice(5,2)
+console.log(letras)
 */
