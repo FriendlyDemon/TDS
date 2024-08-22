55e3aa0f175cd7043d5d5119853189c8a90692dd
senacrs <henriquemichel@live.com>
2024-08-22 16:51:14 -0300

    y

--- UC2/aula8/index.js
+++ UC2/aula8/index.js
@@ -1,4 +1,4 @@
-/*
+/* strings
 const nome = 'henrique'
 console.log(nome.length)
 console.log(nome[0].toUpperCase() + nome.slice(1))
@@ -7,11 +7,12 @@ console.log(email.trim())
 const frase = 'hoje comi cenoura, eu adoro cenoura'
 console.log(frase.replaceAll('cenoura', 'batata'))
 */
-/*
+/* arrays
 const frutas = ['abacate', 'banana', 'tomate', 'milho', 'uva', 'morango'],
 fruta=frutas[4]
 console.log(fruta)
 */
-
+/* array exerc
 cachorros=['bulldog','pitbull','pug','golden retriever','chow-chow']
 console.log(cachorros[require('readline-sync').keyInSelect(cachorros,'Escolha uma raca de cachorro')])
+*/
