d25bd7be0c3536d2571e6e676e6d33ae6d23f554
senacrs <henriquemichel@live.com>
2024-08-22 16:36:43 -0300

    y

--- UC2/aula8/index.js
+++ UC2/aula8/index.js
@@ -1,7 +1,17 @@
-const nome='henrique'
+/*
+const nome = 'henrique'
 console.log(nome.length)
-console.log(nome[0].toUpperCase()+nome.slice(1))
-const email=' hue@tabajara.com '
+console.log(nome[0].toUpperCase() + nome.slice(1))
+const email = ' hue@tabajara.com '
 console.log(email.trim())
-const frase= 'hoje comi cenoura, eu adoro cenoura'
-console.log(frase.replaceAll('cenoura','batata'))
+const frase = 'hoje comi cenoura, eu adoro cenoura'
+console.log(frase.replaceAll('cenoura', 'batata'))
+*/
+/*
+const frutas = ['abacate', 'banana', 'tomate', 'milho', 'uva', 'morango'],
+fruta=frutas[4]
+console.log(fruta)
+*/
+
+cachorros=['bulldog','pitbull','pug','golden retriever','chow-chow']
+console.log(cachorros[require('readline-sync').keyInSelect(cachorros,'Escolha uma raca de cachorro')])
