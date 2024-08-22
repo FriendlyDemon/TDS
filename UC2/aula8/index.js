5894d92f86777c8908a9dffb53f03f2c9ace2bb8
senacrs <henriquemichel@live.com>
2024-08-22 15:53:00 -0300

    y

---
+++ UC2/aula8/index.js
@@ -0,0 +1,7 @@
+const nome='henrique'
+console.log(nome.length)
+console.log(nome[0].toUpperCase()+nome.slice(1))
+const email=' hue@tabajara.com '
+console.log(email.trim())
+const frase= 'hoje comi cenoura, eu adoro cenoura'
+console.log(frase.replaceAll('cenoura','batata'))
