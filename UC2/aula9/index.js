22b63bd1be4af7fb681ed5b8614fa9eaf9a71918
senacrs <henriquemichel@live.com>
2024-08-26 15:01:07 -0300

    y

---
+++ UC2/aula9/index.js
@@ -0,0 +1,20 @@
+/* exe1
+array
+//undefined
+array=null
+//null
+array=[3,4,5,6,7,8,9,10,11,12,13]
+//array.length == 11
+i=0
+//array[i] == 3
+array[i+1]=19
+//4=>19
+valor=array=i+6
+//valor == 9
+*/
+/* exe2
+frase=prompt('Digite uma frase')
+// frase='Subi num ônibus em Marrocos
+console.log(frase.toUpperCase().replace('A','I'), frase.length)
+// SUBI NUM ÔNIBUS EM MIRROCOS 27
+*/
