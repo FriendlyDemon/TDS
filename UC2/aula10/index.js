8a225f185ff1bb6e8ec6d489339759b6c19ef101
senacrs <henriquemichel@live.com>
2024-08-27 14:51:53 -0300

    y

--- UC2/aula10/index.js
+++ UC2/aula10/index.js
@@ -1,6 +1,8 @@
 let num1 = 1, num2 = 2;
 if (num1 == num2) {
     console.log('os números são iguais')
+} else if (num1>num2){
+    console.log('o primeiro número é maior')
 } else {
-    console.log('os números são diferentes')
+    console.log('o segundo número é maior')
 }
