bfa391239bc2ad4d863aa52baa445c5e5cd2db89
senacrs <henriquemichel@live.com>
2024-08-27 16:19:42 -0300

    y

--- UC2/aula10/index.js
+++ UC2/aula10/index.js
@@ -7,7 +7,7 @@ if (num1 == num2) {
 } else {
     console.log('o segundo número é maior')
 }
-
+//
 let paisDeOrigem = 'Brasil'
 
 switch (paisDeOrigem) {
@@ -24,17 +24,28 @@ switch (paisDeOrigem) {
         console.log('pais não encontrado')
         break
 }
-
-let pokemon ='charmander'
-function inicial(){
-switch (pokemon) {
-    case 'charmander':
-        return 'fogo'
-    case 'squirtle':
-        return 'agua'
-    case 'bulbasauro':
-        return 'planta'
-    default:
-        return 'unkown'
-}}
+//
+let pokemon = 'charmander'
+function inicial() {
+    switch (pokemon) {
+        case 'charmander':
+            return 'fogo'
+        case 'squirtle':
+            return 'agua'
+        case 'bulbasauro':
+            return 'planta'
+        default:
+            return 'unkown'
+    }
+}
 console.log(inicial())
+//
+let aluno = 'hermione',
+    alunos = ['narcisa', 'luna', 'ana', 'hermione'],
+    casas = ['sonserina', 'corvinal', 'lufa-lufa', 'grifinória'];
+
+if (alunos.includes(aluno)) {
+    console.log(casas[alunos.indexOf(aluno)])
+} else {
+    console.log('erro')
+}
