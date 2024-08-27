2a8a857911fe1087ad12b31c7eebfa1e35f5acd1
senacrs <henriquemichel@live.com>
2024-08-27 15:54:50 -0300

    y

--- UC2/aula10/index.js
+++ UC2/aula10/index.js
@@ -1,8 +1,40 @@
 let num1 = 1, num2 = 2;
+
 if (num1 == num2) {
     console.log('os números são iguais')
-} else if (num1>num2){
+} else if (num1 > num2) {
     console.log('o primeiro número é maior')
 } else {
     console.log('o segundo número é maior')
 }
+
+let paisDeOrigem = 'Brasil'
+
+switch (paisDeOrigem) {
+    case 'Brasil':
+        console.log('Brasileiro')
+        break
+    case 'Uruguay':
+        console.log('Uruguaio')
+        break
+    case 'para que?':
+        console.log('Paraguaio')
+        break
+    default:
+        console.log('pais não encontrado')
+        break
+}
+
+let pokemon ='charmander'
+function inicial(){
+switch (pokemon) {
+    case 'charmander':
+        return 'fogo'
+    case 'squirtle':
+        return 'agua'
+    case 'bulbasauro':
+        return 'planta'
+    default:
+        return 'unkown'
+}}
+console.log(inicial())
