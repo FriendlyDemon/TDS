d7786e0e3149c31c4f97d22248b27b802ddb9001
senacrs <henriquemichel@live.com>
2024-09-09 16:32:48 -0300

    y

---
+++ UC2/aula15/retorno.js
@@ -0,0 +1,21 @@
+//
+const calcArea = function (largura, altura) {
+    const resultadoArea = largura * altura
+    return resultadoArea
+}
+
+let area = calcArea(2, 5)
+console.log('A area é:', area)
+//
+//
+const soma = function (a, b) { return a + b }
+let resultado = soma(5, 3);
+console.log(resultado)
+
+const arr = (array)[array[0] / 2, array.at(-1) / 2]
+let arrRes = arr([8, 4, 10, 6]);
+console.log(arrRes)
+//
+//
+const calcularArea = (largura, altura) => largura * altura
+//
