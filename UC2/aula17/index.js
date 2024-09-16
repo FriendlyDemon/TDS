f50f76fa9b957436af3a859035134a1dee6345b8
senacrs <henriquemichel@live.com>
2024-09-16 16:21:54 -0300

    y

--- UC2/aula17/index.js
+++ UC2/aula17/index.js
@@ -16,3 +16,5 @@ curso['numeroProfessores']=10
 // console.log(curso.linguagens[0])
 const professores = [{ nome: 'lucas', modulo: 1 }, { nome: 'leo', modulo: 2 }, { nome: 'mika', modulo: 3 }]
 // console.log(professores[0])
+const usuario={nome:'prof',idade:27,email:'prof@senac.com.br'}
+const novoUsuario={...usuario,nome:'lucas',idade:32}
