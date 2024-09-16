564a7f4f32b729c8b2fdb3362424f72eec4dc961
senacrs <henriquemichel@live.com>
2024-09-16 15:17:48 -0300

    y

--- UC2/aula17/exerc.js
+++ UC2/aula17/exerc.js
@@ -1,2 +1,7 @@
-const filme1={diretor:['diretor1','diretor2'],nome:'filme1',ano:'2000',elenco:['ator1','ator2','ator3'],visto:true};
-console.log(filme1.diretor,filme1['nome'],filme1.ano,filme1['elenco'],filme1.visto)
+const fs = require('node:fs')
+const filme1 = { diretor: ['diretor1', 'diretor2'], nome: 'filme1', ano: '2000', elenco: ['ator1', 'ator2', 'ator3'], visto: true };
+// console.log(filme1.diretor,filme1['nome'],filme1.ano,filme1['elenco'],filme1.visto)
+const pessoa = { nome: 'longo', idade: 'muitos', 'genero musical': 'ouvir música' }
+// console.log(`O nome da pessoa é ${pessoa.nome}, ela tem ${pessoa.idade} anos e gosta muito de ${pessoa['genero musical']}`)
+const gato = { nome: 'Ulisses', idade: 4, genero: 'macho' }, henrique = { nome: 'Henrique Michel', idade: 30, genero: 'masculino', gato }
+// console.log(henrique.gato)
--- UC2/aula17/index.js
+++ UC2/aula17/index.js
@@ -4,8 +4,13 @@ const professor = {
     altura: 1.75,
     tarefas: ['dar aula', 'responder duvidas'],
     contarPiadas: function () { console.log('é pave ou pacome?') },
-    'e mail':'lucas@gmail'
+    'e mail': 'lucas@gmail'
 }
-professor.nome='leo'
-console.log(professor['e mail'])
-console.log(professor.nome)
+professor.nome = 'leo'
+// console.log(professor['e mail'])
+// console.log(professor.nome)
+
+const curso = { nome: 'front end noturno', linguagens: ['js', 'css', 'html'] }
+// console.log(curso.linguagens[0])
+const professores = [{ nome: 'lucas', modulo: 1 }, { nome: 'leo', modulo: 2 }, { nome: 'mika', modulo: 3 }]
+// console.log(professores[0])
