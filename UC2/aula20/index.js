b878d986f808e2a0198ed39dad215c5e08997f36
senacrs <henriquemichel@live.com>
2024-09-24 17:18:46 -0300

    y

--- UC2/aula20/index.js
+++ UC2/aula20/index.js
@@ -51,4 +51,57 @@ function sum(mat) {
     }
     return soma
 }
-console.log(sum(exer))
+// console.log(sum(exer))
+
+// batnav:
+const rl = require('readline-sync')
+let agua = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+function navio(mat) {
+    function r(o){return Math.floor(Math.random()*o)}
+
+    let n1=[r(11),r(10)]
+    mat[n1[0]][n1[1]]=1
+    mat[n1[0]][n1[1]+1]=1
+    
+    let n2=[r(10),r(11)]
+    mat[n2[0]][n2[1]]=1
+    mat[n2[0]+1][n2[1]]=1
+
+    let n3=[r(11),r(9)]
+    mat[n3[0]][n3[1]]=1
+    mat[n3[0]][n3[1]+1]=1
+    mat[n3[0]][n3[1]+2]=1
+
+    let n4=[r(9),r(11)]
+    mat[n4[0]][n4[1]]=1
+    mat[n4[0]+1][n4[1]]=1
+    mat[n4[0]+2][n4[1]]=1
+
+    let n5=[r(11),r(8)]
+    mat[n3[0]][n3[1]]=1
+    mat[n3[0]][n3[1]+1]=1
+    mat[n3[0]][n3[1]+2]=1
+    mat[n5[0]][n5[1]+3]=1
+
+    let n6=[r(8),r(11)]
+    mat[n4[0]][n4[1]]=1
+    mat[n4[0]+1][n4[1]]=1
+    mat[n4[0]+2][n4[1]]=1
+    mat[n6[0]+3][n6[1]]=1
+}
+
+{
+rl.questionInt('escolha uma linha: ')
+rl.questionInt('escolha uma coluna: ')
+}
