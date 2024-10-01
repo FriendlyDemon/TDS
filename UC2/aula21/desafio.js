7d53c836c2f50d9a2455e7cb0e761e07a27d83d4
senacrs <henriquemichel@live.com>
2024-10-01 16:16:28 -0300

    y

--- UC2/aula21/desafio.js
+++ UC2/aula21/desafio.js
@@ -1,42 +1,116 @@
-let ilha = [[], [], [], [], [], [], [], [], [], []]
+let ilha = [[], [], [], [], [], [], [], [], [], []],
+    dino = [[], [], [], [], [], [], [], [], [], []]
 
-// 0: "grama"; 1: "água"; 2: "floresta"; 3: "carnívoro"; 4: "herbívoro"
+// 0: "grama"; 1: "água"; 2: "floresta";
+// 0: nada 1: "herbívoro"; 2: "carnívoro";
 
 for (x = 0; x < 10; x++) {
     for (y = 0; y < 10; y++) {
-        ilha[x][y] = Math.floor(Math.random() * 5)
+        ilha[x][y] = Math.floor(Math.random() * 3)
     }
 }
 
-console.log(ilha.join('\n'))
+for (x = 0; x < 10; x++) {
+    for (y = 0; y < 10; y++) {
+        if (ilha[x][y] != 1) {
+            if (Math.floor(Math.random() * 5) == 4) {
+                dino[x][y] = 1
+            } else {
+                dino[x][y] = 0
+            }
+        } else {
+            dino[x][y] = 0
+        }
+    }
+}
+car = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)]
 
-function move(x,y,dir) {
+while (ilha[car[0]][car[1]] == 1 || dino[car[0]][car[1]] == 1) {
+    car = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)]
+}
+dino[car[0]][car[1]] = 2
+
+for (x = 0; x < 10; x++) {
+    if (dino[x].includes(2)) {
+        tyr = [x, dino[x].indexOf(2)]
+    }
+}
+
+terreno.innerHTML = ilha.join('\n');
+dinos.innerHTML = dino.join('\n');
+
+function move(x, y, dir) {
     switch (dir) {
-        case 'cima':if(y!=0){
-            ilha[x-1][y]=ilha[x][y]
-            ilha[x][y]=0
-        }else{
-                console.log('ERRO: Não pode mover para cima')}
+        case 'cima':
+            if (y != 0 && ilha[x - 1][y] != 1) {
+                dino[x - 1][y] = 2
+                dino[x][y] = 0
+                for (x = 0; x < 9; x++) {
+                    if (dino[x].includes(2)) {
+                        tyr = [x, dino[x].indexOf(2)]
+                    }
+                }
+                terreno.innerHTML = ilha.join('\n')
+                dinos.innerHTML = dino.join('\n')
+                dakk.innerText = 'coma'
+            } else {
+                dakk.innerText = 'ERRO: Não pode mover para cima'
+            }
             break;
-        case 'baixo':if(y!=9){
-            ilha[x+1][y]=ilha[x][y]
-            ilha[x][y]=0
-        }else{
-                console.log('ERRO: Não pode mover para baixo')}
+        case 'baixo':
+            if (y != 9 && ilha[x + 1][y] != 1) {
+                dino[x + 1][y] = 2
+                dino[x][y] = 0
+                for (x = 0; x < 9; x++) {
+                    if (dino[x].includes(2)) {
+                        tyr = [x, dino[x].indexOf(2)]
+                    }
+                }
+                terreno.innerHTML = ilha.join('\n')
+                dinos.innerHTML = dino.join('\n')
+                dakk.innerText = 'coma'
+            } else {
+                dakk.innerText = 'ERRO: Não pode mover para baixo'
+            }
             break;
-        case 'esquerda':if(x!=0){
-            ilha[x][y-1]=ilha[x][y]
-            ilha[x][y]=0
-        }else{
-                console.log('ERRO: Não pode mover para esquerda')}
+        case 'esquerda':
+            if (x != 0 && ilha[x][y - 1] != 1) {
+                dino[x][y - 1] = 2
+                dino[x][y] = 0
+                for (x = 0; x < 9; x++) {
+                    if (dino[x].includes(2)) {
+                        tyr = [x, dino[x].indexOf(2)]
+                    }
+                }
+                terreno.innerHTML = ilha.join('\n')
+                dinos.innerHTML = dino.join('\n')
+                dakk.innerText = 'coma'
+            } else {
+                dakk.innerText = 'ERRO: Não pode mover para esquerda'
+            }
             break;
-        case 'direita':if(x!=9){
-            ilha[x][y+1]=ilha[x][y]
-            ilha[x][y]=0
-        }else{
-                console.log('ERRO: Não pode mover para direita')}
+        case 'direita':
+            if (x != 9 && ilha[x][y + 1] != 1) {
+                dino[x][y + 1] = 2
+                dino[x][y] = 0
+                for (x = 0; x < 9; x++) {
+                    if (dino[x].includes(2)) {
+                        tyr = [x, dino[x].indexOf(2)]
+                    }
+                }
+                terreno.innerHTML = ilha.join('\n')
+                dinos.innerHTML = dino.join('\n')
+                dakk.innerText = 'coma'
+            } else {
+                dakk.innerText = 'ERRO: Não pode mover para direita'
+            }
             break;
-        default:console.log('ERRO: direção inválida')
+        default: dakk.innerText = 'ERRO: direção inválida'
             break;
     }
 }
+
+document.getElementById('up').addEventListener('click', () => { move(tyr[0], tyr[1], 'cima') });
+document.getElementById('do').addEventListener('click', () => { move(tyr[0], tyr[1], 'baixo') });
+document.getElementById('le').addEventListener('click', () => { move(tyr[0], tyr[1], 'esquerda') });
+document.getElementById('ri').addEventListener('click', () => { move(tyr[0], tyr[1], 'direita') });
