23838d452cc296857c8952fc97a343df3eb1ba89
senacrs <henriquemichel@live.com>
2024-10-21 14:11:35 -0300

    y

---
+++ UC3/foobar.js
@@ -0,0 +1,9 @@
+//E-mail: assunto: [BD24-2T]Nome-Motivo
+/* avaliações
+a 12/11
+b 27/01
+c all
+d all
+e(recuperação) 3/2
+*sem feriados*
+*/
--- UC4/Trabalho 1/padaria.ts
+++ UC4/Trabalho 1/padaria.ts
@@ -1,19 +1,19 @@
-class Cliente{
-    nome:string
-    compras:number=0
-    carrinho={}
-    addCarrihno(produto:Produto,quantia:number){
-        this.carrinho[produto.nome]=quantia
-    }
+class Cliente {
+  nome: string;
+  compras: number = 0;
+  carrinho = {};
+  addCarrihno(produto: Produto, quantia: number) {
+    this.carrinho[produto.nome] = quantia;
+  }
 }
-class Produto{
-    nome:string
-    valor:number
-    ingrediente:Array<string>
+class Produto {
+  nome: string;
+  valor: number;
+  ingrediente: Array<string>;
 }
-let estoque={
-    farinha:0,
-    acucar:0,
-    ovos:0,
-    leite:0
-}
+let estoque = {
+  farinha: 0,
+  acucar: 0,
+  ovos: 0,
+  leite: 0,
+};
