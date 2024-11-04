1a2deca92861fe281695c2a9ba40fdcc5dfb92b4
senacrs <henriquemichel@live.com>
2024-11-04 13:46:11 -0300

    y

--- hue.js
+++
@@ -1,2 +0,0 @@
-let hue = {a:'a',b:'b',c:'c'}
-console.log(Object.keys(hue).includes('a'))
--- test.js
+++ test.js
@@ -1,4 +1,10 @@
+/*
 let dakka = '0'
 if (dakka > 0) {
     console.log('sim')
 } else { console.log('não') }
+*/
+/*
+let hue = {a:'a',b:'b',c:'c'}
+console.log(Object.keys(hue).includes('a'))
+*/
