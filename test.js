35079b39511f25399c6fe17264e9f0fc113a30f9
senacrs <henriquemichel@live.com>
2024-11-04 13:53:57 -0300

    y

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-/*
+/* number as string still can be compared vs number (<=>)
 let dakka = '0'
 if (dakka > 0) {
     console.log('sim')
 } else { console.log('não') }
 */
-/*
+/* how to know if an obj has a property
 let hue = {a:'a',b:'b',c:'c'}
 console.log(Object.keys(hue).includes('a'))
 */
