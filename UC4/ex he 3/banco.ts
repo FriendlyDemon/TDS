be4133fb98bdd5ddb91990516f65f43ae33159f0
senacrs <henriquemichel@live.com>
2024-11-14 16:14:41 -0300

    y

---
+++ UC4/ex he 3/banco.ts
@@ -0,0 +1,11 @@
+class conta{
+    saldo:number
+    taxaJuros:number
+    limiteSaque:number
+    transferir(quanto,quem){
+        ''
+    }
+}
+class corrente extends conta{}
+class poupanca extends conta{}
+class investimento extends conta{}
