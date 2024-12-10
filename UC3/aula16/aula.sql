cb7855a5d328e234ae19aed0e5898cfcda4f7fcf
senacrs <henriquemichel@live.com>
2024-12-10 17:20:16 -0300

    y

---
+++ UC3/aula16/aula.sql
@@ -0,0 +1,31 @@
+select produto.nome from pedido
+join produto on produto.id=pedido.fk_produto
+join cliente on cliente.id=pedido.fk_cliente
+where cliente.id=5
+
+select nome from produto
+where categoria=1
+
+select produto.nome from pedido
+join produto on produto.id=pedido.fk_produto
+join cliente on cliente.id=pedido.fk_cliente
+where cliente.id=5 and produto.categoria=4
+
+select produto.nome from pedido
+join produto on produto.id=pedido.fk_produto
+join cliente on cliente.id=pedido.fk_cliente
+where cliente.id=5
+
+--
+
+select count(produto.id),categoria.nome from categoria
+left join produto on categoria.id=produto.categoria
+group by categoria.id
+
+select pedido.id as pedido,cliente.nome as cliente,pedido.quantidade from cliente
+right join pedido on pedido.fk_cliente=cliente.id
+
+select produto.nome from produto
+cross join categoria
+where categoria.nome='brinquedos'
+
