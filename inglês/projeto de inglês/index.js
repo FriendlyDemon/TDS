49dbb8338f6877ede7a8a005c731eb23852da840
senacrs <henriquemichel@live.com>
2024-12-11 14:40:15 -0300

    y

--- inglês/projeto de inglês/index.js
+++ inglês/projeto de inglês/index.js
@@ -1,8 +1,14 @@
-function linkimagem(arg){
-    alert('texto')
+function linkimagem4(arg){
 if(arg.checked){
         img4.src=arg.value
     } else {
         img4.src=''
     }
 }
+function linkimagem3(arg){
+    if(arg.checked){
+            img3.src=arg.value
+        } else {
+            img3.src=''
+        }
+    }
