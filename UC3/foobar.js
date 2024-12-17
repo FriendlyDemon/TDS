ab7d7177eed56e4f52d93ed73baad6cf440fa75f
senacrs <henriquemichel@live.com>
2024-12-17 14:55:56 -0300

    y

--- UC3/foobar.js
+++ UC3/foobar.js
@@ -40,5 +40,5 @@ where type='murder' and date=20180115
 
 https://mystery.knightlab.com/
 
-'Security footage shows that there were 2 witnesses. The first witness lives at the last house on "Northwestern Dr". The second witness, named Annabel, lives somewhere on "Franklin Ave".'
+Miranda Priestly and Jeremy Bowers
 */
