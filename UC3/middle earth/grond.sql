733ad5842a6307245e3504c62f594cc3e67adf31
FriendlyDemon <henriquemichel@live.com>
2025-02-03 16:23:21 -0300

    y

--- UC3/middle earth/grond.sql
+++ UC3/middle earth/grond.sql
@@ -22,11 +22,49 @@ create table
     );
 
 --
+insert into
+    personagem (nome)
+values
+    ('Aragorn'),
+    ('Legolas'),
+    ('Gimli'),
+    ('Gandalf'),
+    ('Tom Bombadil'),
+    ('Galadriel'),
+    ('Shelob');
 
-insert into personagem(nome) values ('Aragorn'),('Legolas'),('Gimli'),('Gandalf'),('Tom Bombadil'),('Galadriel'),('Shelob');
+insert into
+    cenario (nome)
+values
+    ('Floresta de Lothlórien'),
+    ('Montanhas de Moria'),
+    ('Campo de Batalha de Helm'),
+    ('Isengard'),
+    ('Portões de Mordor'),
+    ('Valinor'),
+    ('Condado');
 
-insert into cenario(nome) values ('Floresta de Lothlórien'),('Montanhas de Moria'),('Campo de Batalha de Helm'),('Isengard'),('Portões de Mordor'),('Valinor'),('Condado');
+insert into
+    item (nome, tipo)
+values
+    ('Espada Elfica', 'espada'),
+    ('Arco Longo', 'arco'),
+    ('Machado de Batalha', 'machado'),
+    ('Varinha Mágica', 'focus'),
+    ('Anduril', 'espada'),
+    ('Lamina Morgul', 'faca'),
+    ('Ferroada', 'espada'),
+    ('O Anél', 'anél');
 
-insert into item(nome, tipo) values ('Espada Elfica','espada'),('Arco Longo','arco'),('Machado de Batalha','machado'),('Varinha Mágica','focus'),('Anduril','espada'),('Lamina Morgul','faca'),('Ferroada','espada'),('O Anél','anél');
-
-insert into coletado(fk_personagem,fk_item,fk_cenario) values (22,17,22),(23,18,23),(24,19,24),(25,20,22),(22,19,24),(23,17,22),(26,24,24),(28,23,26),(27,17,22);
+insert into
+    coletado (fk_personagem, fk_item, fk_cenario)
+values
+    (22, 17, 22),
+    (23, 18, 23),
+    (24, 19, 24),
+    (25, 20, 22),
+    (22, 19, 24),
+    (23, 17, 22),
+    (26, 24, 24),
+    (28, 23, 26),
+    (27, 17, 22);
---
+++ UC3/simulado.sql
@@ -0,0 +1,90 @@
+create database hogwash;
+
+create table
+    house (id serial primary key, name varchar(50));
+
+create table
+    student (
+        id serial primary key,
+        name varchar(100),
+        house_id int,
+        foreign key (house_id) references house (id)
+    );
+
+create table
+    subject (id serial primary key, name varchar(50));
+
+create table
+    teacher (
+        id serial primary key,
+        name varchar(100),
+        house_id int,
+        foreign key (house_id) references house (id)
+    );
+
+create table
+    teacher_subject (
+        id serial primary key,
+        teacher_id int,
+        subject_id int,
+        foreign key (teacher_id) references teacher (id),
+        foreign key (subject_id) references subject (id)
+    );
+
+create table
+    spell (
+        id serial primary key,
+        name varchar(50),
+        subject_id int,
+        foreign key (subject_id) references subject (id)
+    );
+
+insert into
+    houses (name)
+values
+    ("griffingdor"),
+    ("slitherin"),
+    ("hufflepuff"),
+    ("ravenclaw");
+
+insert into
+    student (name, house_id)
+values
+    ("Harry Potter", 1),
+    ("Draco Malfoy", 2),
+    ("Luna Lovegood", 3),
+    ("Margaret Thatcher", 4);
+
+insert into
+    subject (name)
+values
+    ("potions"),
+    ("defence"),
+    ("offence"),
+    ("beasts");
+
+insert into
+    teacher (name)
+values
+    ("Severus Snape",2),
+    ("Minerva McGonagall",4),
+    ("Remo Lupin",3),
+    ("Rúbeo Hagrid",1);
+
+insert into
+    teacher_subject (teacher_id, subject_id)
+values
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (1, 2);
+
+insert into
+    spell (name, subject_id)
+values
+    ("Avada Kedavra", 3),
+    ("Stupefy", 3),
+    ("Wingardium Leviosa", 2),
+    ("Expeliarmus", 2);
+
