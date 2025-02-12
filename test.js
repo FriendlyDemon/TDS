975b3bbc01584865245855de496966d81e66a9ea
FriendlyDemon <henriquemichel@live.com>
2025-02-12 15:18:26 -0300

    y

--- test.js
+++ test.js
@@ -21,6 +21,8 @@ fs.readFile("./file.txt", function (erro, texto) {
 });
 */
 
+// npm install --save-dev @types/node -- to use node modules in ts
+
 // typescript constructor generator
 
 // https://code.visualstudio.com/api/get-started/your-first-extension
