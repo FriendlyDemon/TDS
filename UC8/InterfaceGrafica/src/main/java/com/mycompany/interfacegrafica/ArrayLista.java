9a7bb2945e9549133017d1d8f61fad0cba274e52
FriendlyDemon <henriquemichel@live.com>
2025-03-17 17:16:42 -0300

    y

---
+++ UC8/InterfaceGrafica/src/main/java/com/mycompany/interfacegrafica/ArrayLista.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.interfacegrafica;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ArrayLista {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(60);
+        numeros.add(90);
+        numeros.add(315);
+        numeros.remove(1);
+        System.out.println(numeros);
+    }
+}
