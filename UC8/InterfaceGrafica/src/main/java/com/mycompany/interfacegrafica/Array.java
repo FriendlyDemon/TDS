69a46f9f66fa0dd7721e654cfc2c0b1c6f0b0a76
FriendlyDemon <henriquemichel@live.com>
2025-03-17 16:51:39 -0300

    y

---
+++ UC8/InterfaceGrafica/src/main/java/com/mycompany/interfacegrafica/Array.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.interfacegrafica;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Array {
+
+    public Array() {
+
+    }
+
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+        int[] numerico = new int[5];
+        numerico[0] = 10;
+        numerico[1] = 22;
+        numerico[2] = 55;
+        numerico[3] = 78;
+        numerico[4] = 100;
+        int[] nununu = new int[]{1,2,3,4,5};
+        System.out.println(Arrays.toString(numeros));
+    }
+}
