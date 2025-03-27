ce18ede9eb75b0ee832380f9458ee04e9ad4a5ce
FriendlyDemon <henriquemichel@live.com>
2025-03-27 17:18:19 -0300

    y

---
+++ UC9/BancoDeDados/src/main/java/com/mycompany/bancodedados/BancoDeDados.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.bancodedados;
+
+/**
+ *
+ * @author HENRIQUEMICHEL
+ */
+public class BancoDeDados {
+
+    public static void main(String[] args) {
+        ConexaoSQLite.conectar();
+    }
+}
---
+++ UC9/BancoDeDados/src/main/java/com/mycompany/bancodedados/ConexaoSQLite.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bancodedados;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author HENRIQUEMICHEL
+ */
+public class ConexaoSQLite {
+
+    public static Connection conectar() {
+        Connection conexao = null;
+        String url = "jdbc:sqlite:usuarios.db";
+        try {
+            conexao = DriverManager.getConnection(url);
+            System.out.println("Conexão com SQLite estabelecida!");
+
+        } catch (SQLException e) {
+            System.out.print("Erro ao conectar ao banco" + e.getMessage());
+        }
+        return conexao;
+    }
+
+    public static void descpmectar(Connection conexao) {
+        try {
+            if (conexao != null) {
+                conexao.close();
+                System.out.println("Conexao fechada.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao fechar a conexão:" + e.getMessage());
+        }
+    }
+}
