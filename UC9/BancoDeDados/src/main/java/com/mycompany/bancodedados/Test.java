7db525dfac30e4e9153453e859a66dc6163b2228
FriendlyDemon <henriquemichel@live.com>
2025-04-01 17:20:43 -0300

    y

--- UC9/BancoDeDados/src/main/java/com/mycompany/bancodedados/Test.java
+++ UC9/BancoDeDados/src/main/java/com/mycompany/bancodedados/Test.java
@@ -14,6 +14,14 @@ public class Test extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         bemVindo = new javax.swing.JLabel();
+        nome = new javax.swing.JTextField();
+        email = new javax.swing.JTextField();
+        senha = new javax.swing.JPasswordField();
+        atualizar = new javax.swing.JButton();
+        deletar = new javax.swing.JButton();
+        lNome = new javax.swing.JLabel();
+        lEmail = new javax.swing.JLabel();
+        lSenha = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(546, 460));
@@ -24,21 +32,63 @@ public class Test extends javax.swing.JFrame {
         bemVindo.setText("Seja bem vindo(a) Anon!");
         bemVindo.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
+        atualizar.setText("Atualizar");
+
+        deletar.setText("Deletar");
+
+        lNome.setText("Nome");
+
+        lEmail.setText("E-mail");
+
+        lSenha.setText("Senha");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(169, 169, 169)
+                .addGap(156, 156, 156)
                 .addComponent(bemVindo, javax.swing.GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE)
-                .addGap(186, 186, 186))
+                .addGap(199, 199, 199))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(71, 71, 71)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(lNome)
+                    .addComponent(lEmail)
+                    .addComponent(lSenha))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(atualizar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 88, Short.MAX_VALUE)
+                        .addComponent(deletar))
+                    .addComponent(nome)
+                    .addComponent(email)
+                    .addComponent(senha))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(158, 158, 158)
+                .addGap(73, 73, 73)
                 .addComponent(bemVindo, javax.swing.GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE)
-                .addGap(283, 283, 283))
+                .addGap(72, 72, 72)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lNome))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(email, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lEmail))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(senha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lSenha))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(atualizar)
+                    .addComponent(deletar))
+                .addGap(153, 153, 153))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -56,7 +106,15 @@ public class Test extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton atualizar;
     private javax.swing.JLabel bemVindo;
+    private javax.swing.JButton deletar;
+    private javax.swing.JTextField email;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lEmail;
+    private javax.swing.JLabel lNome;
+    private javax.swing.JLabel lSenha;
+    private javax.swing.JTextField nome;
+    private javax.swing.JPasswordField senha;
     // End of variables declaration//GEN-END:variables
 }
