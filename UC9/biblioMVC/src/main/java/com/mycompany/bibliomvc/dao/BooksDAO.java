0d2a2a6180c9e00b1a3245a6261fee10eb5f72e3
FriendlyDemon <henriquemichel@live.com>
2025-04-03 17:29:40 -0300

    y

--- UC9/biblioMVC/src/main/java/com/mycompany/bibliomvc/dao/BooksDAO.java
+++ UC9/biblioMVC/src/main/java/com/mycompany/bibliomvc/dao/BooksDAO.java
@@ -18,17 +18,34 @@ public class BooksDAO {
 
     public static void addBook(Connection connection, Book book) {
         String sql = "INSERT INTO books (title,author,price,year) values (?,?,?,?)";
-        
+
         try (PreparedStatement dakka = connection.prepareStatement(sql)) {
             dakka.setString(1, book.getAuthor());
             dakka.setString(2, book.getAuthor());
             dakka.setDouble(3, book.getPrice());
             dakka.setInt(4, book.getYear());
-            
+
             dakka.executeUpdate();
+            System.out.println("Sucess");
 
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
+    public static void updateBook(Connection connection, int id, String title, String author, double price, int year) {
+        String sql = "UPDATE books SET title=?, author=?, price=?, year=? WHERE id=?";
+        try (PreparedStatement dakka = connection.prepareStatement(sql)) {
+            dakka.setString(1, title);
+            dakka.setString(2, author);
+            dakka.setDouble(3, price);
+            dakka.setInt(4, year);
+            dakka.setInt(5, id);
+
+            dakka.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
