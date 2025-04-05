2bed09a89e866f55cff5780e7cf7990da51ad280
FriendlyDemon <henriquemichel@live.com>
2025-04-05 12:41:05 -0300

    y

---
+++ UC9/TestFrame/src/main/java/com/mycompany/testframe/TestFrame.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.testframe;
+
+/**
+ *
+ * @author henri
+ */
+public class TestFrame {
+
+    public static void main(String[] args) {
+        NewJFrame frame = new NewJFrame();
+        frame.setContentPane(new JPanel1());
+        frame.setVisible(true);
+    }
+}
