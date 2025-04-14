86df307d7a3b101893346603ea0ff818b39f9d20
FriendlyDemon <henriquemichel@live.com>
2025-04-14 17:23:28 -0300

    y

---
+++ UC9/LoginHash/src/main/java/com/mycompany/loginhash/LoginHash.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginhash;
+
+import com.mycompany.loginhash.database.ConnectionSQL;
+
+/**
+ *
+ * @author HENRIQUEMICHEL
+ */
+public class LoginHash {
+
+    public static void main(String[] args) {
+        ConnectionSQL.conectar();
+    }
+}
