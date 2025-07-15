3490c82cbf82d42479a1f84add11d78f8ca11949
FriendlyDemon <henriquemichel@live.com>
2025-07-15 15:43:43 -0300

    aula 15-07

--- UC12/exercicio_portfolio/script.js
+++ UC12/exercicio_portfolio/script.js
@@ -1,47 +1,21 @@
-const t1 = document.getElementById("T1");
-const t2 = document.getElementById("T2");
-const t3 = document.getElementById("T3");
-const d1 = document.getElementById("D1");
-const d2 = document.getElementById("D2");
-const d3 = document.getElementById("D3");
 const menu = document.getElementById("show");
 const nav = document.querySelector("nav");
+const card_btns = document.querySelectorAll(".card-btn");
+const infos = document.querySelectorAll(".info");
 
-t1.addEventListener("click", () => {
-  t1.classList.toggle("pressed");
-  t1.classList.toggle("not-pressed");
-  if (d1.style.maxHeight < "500px") {
-    d1.style.maxHeight = "500px";
-    t1.textContent = "Show Less";
-  } else {
-    d1.style.maxHeight = "0px";
-    t1.textContent = "Show More";
-  }
-});
-
-t2.addEventListener("click", () => {
-  t2.classList.toggle("pressed");
-  t2.classList.toggle("not-pressed");
-  if (d2.style.maxHeight < "500px") {
-    d2.style.maxHeight = "500px";
-    t2.textContent = "Show Less";
-  } else {
-    d2.style.maxHeight = "0px";
-    t2.textContent = "Show More";
-  }
-});
-
-t3.addEventListener("click", () => {
-  t3.classList.toggle("pressed");
-  t3.classList.toggle("not-pressed");
-  if (d3.style.maxHeight < "500px") {
-    d3.style.maxHeight = "500px";
-    t3.textContent = "Show Less";
-  } else {
-    d3.style.maxHeight = "0px";
-    t3.textContent = "Show More";
-  }
-});
+for (let i = 0; i < card_btns.length; i++) {
+  card_btns[i].addEventListener("click", () => {
+    card_btns[i].classList.toggle("pressed");
+    card_btns[i].classList.toggle("not-pressed");
+    if (infos[i].style.maxHeight < "500px") {
+      infos[i].style.maxHeight = "500px";
+      card_btns[i].textContent = "Show Less";
+    } else {
+      infos[i].style.maxHeight = "0px";
+      card_btns[i].textContent = "Show More";
+    }
+  });
+}
 
 menu.addEventListener("click", () => {
   nav.classList.toggle("ativo");
