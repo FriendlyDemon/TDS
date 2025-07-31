7e45b93346c572c0d7173022e824ac506b592766
FriendlyDemon <henriquemichel@live.com>
2025-07-31 14:04:13 -0300

    aula 31-07

---
+++ UC12/Tailwind/meu-projeto/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+      <button className="text-gray-100 bg-green-300 p-5 rounded-md font-semibold cursor-pointer shadow-md">
+        click
+      </button>
+    </div>
+  );
+}
+
+export default App;
