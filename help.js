a9731cea61692ac50a485b3a8c1673fee615eafa
FriendlyDemon <henriquemichel@live.com>
2025-08-22 15:25:20 -0300

    aula 22-08

--- help.js
+++ help.js
@@ -1,4 +1,6 @@
-for (x = 0; x < 1;) {
-    eval(zas = require('readline-sync').question(': '))
-    if (zas == 0) { x++ }
-};
+for (x = 0; x < 1; ) {
+  eval((zas = require("readline-sync").question(": ")));
+  if (zas == 0) {
+    x++;
+  }
+}
