2f7e974db6e084fdbb0a4147565eb200ac22b719
FriendlyDemon <henriquemichel@live.com>
2025-11-10 15:32:39 -0300

    aula 10-11

--- UC15/05-11/MeuApp/App.tsx
+++ UC15/05-11/MeuApp/App.tsx
@@ -41,3 +41,61 @@ export default function App() {
     </SafeAreaProvider>
   );
 }
+
+/*
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName: keyof typeof Ionicons.glyphMap = "home-outline";
+            if (route.name === "Home") iconName = "home-outline";
+            if (route.name === "About") iconName = "help-outline";
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="About" component={AboutScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+} 
+*/
+/*
+  const Tab = createBottomTabNavigator<StackParamList>();
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName: keyof typeof Ionicons.glyphMap = "home-outline";
+            if (route.name === "Home") iconName = "home-outline";
+            if (route.name === "About") iconName = "help-outline";
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="About" component={AboutScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+*/
+/*
+  const Stack = createNativeStackNavigator<StackParamList>();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+ */
+//<Stack.Navigator initialRouteName="Home"><Stack.Screen name="Home" component={HomeScreen}/><Stack.Screen name="Profile" component={ProfileScreen}/></Stack.Navigator>
